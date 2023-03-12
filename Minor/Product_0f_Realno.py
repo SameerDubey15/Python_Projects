6e742d973131704fa659fcbce9735f2cef53c678
Sameer Dubey <101457667+SameerDubey15@users.noreply.github.com>
2023-03-12 12:19:21 +0530

    Delete Minor directory

--- Minor/Product_0f_Realno.py
+++
@@ -1,9 +0,0 @@
-num=int(input("Enter the numbers of real number that has to be multiplied\n"))
-realno=[]
-mul=1
-for i in range(num):
-    real=float(input("Enter the Real Number: "))
-    realno.append(real)
-    mul=mul*real
-
-print(f"The product of {realno} is {mul}")
